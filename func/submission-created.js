646a792153e5e7818528d37e2584091335189235
Discord Servers <ole.martin277@gmail.com>
2023-08-19 20:40:32 +0200

    Update submission-created.js

--- func/submission-created.js
+++ func/submission-created.js
@@ -33,22 +33,6 @@ export async function handler(event, context) {
         payload.level !== undefined && 
         payload.token !== undefined) {
         
-        // Check if the user ID is already in the ban_appeal_submissions collection
-        const isAlreadySubmitted = await checkIfAlreadySubmitted(userInfo.id);
-    
-        if (isAlreadySubmitted) {
-            // Calculate the time remaining before the user can submit again (3 weeks)
-            const currentTime = new Date();
-            const threeWeeksFromNow = new Date(currentTime.getTime() + (3 * 7 * 24 * 60 * 60 * 1000)); // 3 weeks in milliseconds
-    
-            return {
-                statusCode: 303,
-                headers: {
-                    "Location": `/error?msg=${encodeURIComponent(`You have already submitted a ban appeal. Please wait until ${threeWeeksFromNow.toISOString()} before submitting another.`)}`,
-                },
-            };
-        }
-        
         const message = {
             embed: {
                 title: "New appeal submitted!",
@@ -97,6 +81,23 @@ export async function handler(event, context) {
                 };
             }
         }
+
+    // Check if the user ID is already in the ban_appeal_submissions collection
+    const isAlreadySubmitted = await checkIfAlreadySubmitted(userInfo.id);
+
+    if (isAlreadySubmitted) {
+        // Calculate the time remaining before the user can submit again (3 weeks)
+        const currentTime = new Date();
+        const threeWeeksFromNow = new Date(currentTime.getTime() + (3 * 7 * 24 * 60 * 60 * 1000)); // 3 weeks in milliseconds
+
+        return {
+            statusCode: 303,
+            headers: {
+                "Location": `/error?msg=${encodeURIComponent(`You have already submitted a ban appeal. Please wait until ${threeWeeksFromNow.toISOString()} before submitting another.`)}`,
+            },
+        };
+    }
+
 
         const result = await fetch(`${API_ENDPOINT}/channels/${encodeURIComponent(process.env.APPEALS_CHANNEL)}/messages`, {
             method: "POST",
